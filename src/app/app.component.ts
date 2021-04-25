cc84db0ad4cbf7c1deed4d7e216a55fa8023c3dc
mariaparfenyuk <umme0807@gmail.com>
2021-04-25 19:53:01 +0300

    30 minutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-youtube';
+  appTitle = 'Angular todo application'
+
+
+
 }
+
+
